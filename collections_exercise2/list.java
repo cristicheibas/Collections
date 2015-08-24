e54660032356910f76eebb153d72e07ba15315d2
CRISTIAN CHEIBAS <cristicheibas@hotmail.com>
2015-08-24 10:55:48 +0300

    Improved Readability
    added spacing where necessary, improved javadocs

--- collections_exercise2/list.java
+++ collections_exercise2/list.java
@@ -1,88 +1,102 @@
 package collections_exercise2;
 
-/** List Manipulation functions
- * @author ccheibas
+/** 
+ * List Manipulation class with relevant functions
  * first stands for the first element, last for the last one
  * the size is used to know the general weight of the object
  * and make sure the user doesn't try to remove an non-existing object
+ * @author ccheibas
  */
 
-
-public class list{
+public class List {
 	private Node first;
 	private Node last;
 	private int size = 0;
 
-	private class Node{ // Defining a list Node as a class
+	private class Node { 
+		// Defining a list Node as a class
 		String str ;
 		Node next ;
 	}
 
-	public boolean isEmpty (){
+	public boolean isEmpty () {
 		return first == null;  //checks if the list is empty
 	}
 
-	/** Adds an element to the List 
-	 * @author ccheibas
+	/** 
+	 * Adds an element to the List 
 	 * Links the new element to the last Node, 
-	 * increases the size and saves the String in the Node
+	 * increases the size and saves the String in the Node 
+	 * @author ccheibas
 	 */
-	
-	public void add ( String element ) 
-	{
+
+	public void add ( String element ) {
 		Node x = new Node ();
 		x. str = element ;
 		x. next = null ;
 		size++;
-		if (isEmpty()){ 
+		if (isEmpty()) { 
+			//if the queue is empty nullify it
 			first = x; 
-			last = x;		//if the queue is empty nullify it
+			last = x;		
 		}
-		else{
+		else {
+			//else add the element to the last position
 			last.next = x;
 			last = x; 
-		}		// else add the element to the last position
+		}		
 	}
 
-	/**Function used to remove the n-th element of the list.
-	 * @author ccheibas
+	/**
+	 * Function used to remove the n-th element of the list. 
 	 * In order to be able to remove an element from a list we need to have a placeholder for the previous one.
 	 * Namely previous and current, which are used as variables below.
+	 * @author ccheibas
 	 */
-	
-	public void remove (int position){
-		if ((isEmpty())||(position >= size)) 
-			return;//if the queue is empty |OR| the number is greater than the size do nothing
+
+	public void remove (int position) {
+		if ((isEmpty()) || (position >= size)) {
+			//if the queue is empty |OR| the number is greater than the size do nothing
+			return;
+		}
 		if (position == 0) {
-			first=first.next; //if we want to remove the first element this will save us from NullPointerException
+			//if we want to remove the first element this will save us from NullPointerException
+			first = first.next; 
 			return;
 		}
 		size--; //the size gets smaller once we remove an element
-		Node previous = null;//previous element of the iterative base - current
-		Node current = first;//will help us iterate through the list without damaging anything
-		for (int i=0;i<position;i++){
+		Node previous = null; //previous element of the iterative base - current
+		Node current = first; //will help us iterate through the list without damaging anything
+
+		for (int i = 0; i < position; i++) {
+			//elements take the next elements place until we get to the required position in the list
 			previous = current;
-			current = current.next; //elements take the next elements place until we get to the required position in the list
+			current = current.next; 
 		}
-		previous.next = current.next; //the only reason we needed previous for, to be able to remove the link between Nodes once we are done iterating.
+		//the only reason we needed "previous" for, to be able to remove the link between Nodes once we are done iterating.
+		previous.next = current.next; 
 	}
 
-	/** Function used to print the list
+	/** 
+	 * Function used to print the list
+	 * removes the need for getters and setters when printing
 	 * @author ccheibas
-	 * Didn't want to add a ton of getters and setters and create more spagetti code
-	 * So i decided to add a method for printing the entire List
 	 */
 
-	public String toString(){
-		String output ="";
-		if(isEmpty()) 
+	public String toString() {
+		String output = "";
+		if(isEmpty()) {
 			return "I am printing an empty list";
-		Node temp = first;//wouldn't want to ruin the entire list if this is called in the middle of the program
-		while(temp.next!=null){ //as long as the next element exists we add its' content to the overall string
-			output += temp.str+"\n";
-			temp = temp.next;  //eventually we will run out of Nodes and the loop will break.
 		}
-		output+=last.str; //add the last piece since its' out of the loop
+		//wouldn't want to ruin the entire list if this is called in the middle of the program
+		Node temp = first;
+		while(temp.next != null) { 
+			//as long as the next element exists we add its' content to the overall string
+			output += temp.str + "\n";
+			temp = temp.next;  
+			//eventually we will run out of Nodes and the loop will break.
+		}
+		output += last.str; //add the last piece since its' out of the loop
 		return output;
 	}
 }
--- collections_exercise2/main.java
+++ collections_exercise2/main.java
@@ -3,31 +3,31 @@ package collections_exercise2;
 
 
 /**
- * @author ccheibas
  * Implement your own List data structure, 
  * which will allow to access and remove objects (String type). 
  * It has to contain the following methods: 
  * public void add(String element) and public String get(int i).
+ * @author ccheibas
  */
 
 
-public class main {
+public class Main {
 
 	public static void main(String[] args) {
-		list test_obj = new list(); //creates the LIST object
-		test_obj.add("add#0");
-		test_obj.add("add#1");
-		test_obj.add("add#2");
-		test_obj.add("add#3");
-		test_obj.add("add#4"); 
-		test_obj.add("add#5"); 
-		test_obj.add("add#6");
-		test_obj.add("add#7");
-		test_obj.remove(1);
-		test_obj.remove(4);
-		test_obj.remove(0);
+		List listy = new List(); //creates the LIST object
+		listy.add("add#0");
+		listy.add("add#1");
+		listy.add("add#2");
+		listy.add("add#3");
+		listy.add("add#4"); 
+		listy.add("add#5"); 
+		listy.add("add#6");
+		listy.add("add#7");
+		listy.remove(1);
+		listy.remove(4);
+		listy.remove(0);
 
-		System.out.println(test_obj); //printing the list
+		System.out.println(listy); //printing the list
 	}
 }
 
