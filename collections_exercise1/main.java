dec4aa9836d69ecb34a466971b627103f18f7c63
CRISTIAN CHEIBAS <cristicheibas@hotmail.com>
2015-08-24 10:55:48 +0300

    Improved Readability
    added spacing where necessary, improved javadocs

--- collections_exercise1/main.java
+++ collections_exercise1/main.java
@@ -1,36 +1,37 @@
 package collections_exercise1;
 
-
-
 /**
- * @author ccheibas
- * Implement your own stack data structure (LIFO) of strings. 
- * It has to contain the following methods: 
+ * Implement your own stack data structure (LIFO) of strings.
+ * It has to contain the following methods:
  * public void push(String element) and public String pop().
+ * @author ccheibas
  */
 
-
-public class main {
+public class Main {
 
 	public static void main(String[] args) {
-		stack test_obj = new stack(); //creates the stack object
-		test_obj.push("Push#1");
-		test_obj.push("Push#2");
-		test_obj.push("Push#3");
-		test_obj.pop();
-		test_obj.push("Push#4"); 
-		test_obj.push("Push#5");   //playing around with the array for testing purposes
-		test_obj.pop();
-		test_obj.push("Push#6");
-		test_obj.push("Push#7");
-		test_obj.pop();
-		test_obj.push("Push#8");
-		test_obj.push("Push#9");
-		test_obj.push("Push#10");
-		test_obj.pop();
-		test_obj.pop();
-		for(int i=0;i<test_obj.getstack().length;i++)
-		System.out.println(test_obj.getstack()[i]); //printing what's left of the array
+		Stack stacky = new Stack(); //creates the stack object
+		stacky.push("Push#1");
+		stacky.push("Push#2");
+		stacky.push("Push#3");
+		stacky.pop();
+		stacky.push("Push#4");
+		stacky.push("Push#5");
+		//playing around with the array for testing purposes
+		stacky.pop();
+		stacky.push("Push#6");
+		stacky.push("Push#7");
+		stacky.pop();
+		stacky.push("Push#8");
+		stacky.push("Push#9");
+		stacky.push("Push#10");
+		stacky.pop();
+		stacky.pop();
+
+		for(int i = 0; i < stacky.getstack().length; i++) {
+			//printing what's left of the array
+			System.out.println(stacky.getstack()[i]);
+		}
 	}
 }
 
--- collections_exercise1/stack.java
+++ collections_exercise1/stack.java
@@ -1,30 +1,32 @@
 package collections_exercise1;
 
-/** The class responsible for the stack's operations
+/**
+ * A class describing a stack and responsible for its' operations
+ * LIFO means the last one in the first one out.
  * @author ccheibas
- * consists of the stack itself as a private variable with a getter.
- * as well as two methods to push and pop elements. LIFO means the last one in the first one out.
  */
-public class stack {
-	
+
+public class Stack {
 	private String[] stack_array = new String[0];
-	
-	public void push(String element){ //last in - will extend the array one more cell and add the element to it to position 0
-		int new_size = stack_array.length+1; 
-		String [] temp_array = this.stack_array; //dummy variable used for dynamic memory allocation
+	public void push(String element) {
+		//last in - will extend the array one more cell and add the element to it to position 0
+		int new_size = stack_array.length + 1;
+		String [] temp_array = this.stack_array;
+		//dummy variable used for dynamic memory allocation
 		this.stack_array = new String[new_size];
-		stack_array[0]=element;
-		System.arraycopy(temp_array, 0, stack_array, 1, new_size-1); //copies the array starting from temp[0] to stack_array[1]
+		stack_array[0] = element;
+		//copies the array starting from temp[0] to stack_array[1]
+		System.arraycopy(temp_array, 0, stack_array, 1, new_size - 1);
 	}
-	
-	public void pop(){//first out - will remove the element in position 0 and allocate space for the now smaller array
-		int new_size = stack_array.length-1;
+	public void pop() {
+		//first out - will remove the element in position 0 and allocate space for the now smaller array
+		int new_size = stack_array.length - 1;
 		String [] temp_array = this.stack_array;
 		this.stack_array = new String[new_size];
-		System.arraycopy(temp_array, 1, stack_array, 0, new_size); //copies the array starting from temp[1] to stack_array[0]
+		//copies the array starting from temp[1] to stack_array[0]
+		System.arraycopy(temp_array, 1, stack_array, 0, new_size);
 	}
-	
-	public String[] getstack(){
+	public String[] getstack() {
 		return this.stack_array;
 	}
 }
